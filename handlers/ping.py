da4093a2c1f4384e8a18f8b699950069e96ede7e
It's Hussain <90657846+The-Death-Soul@users.noreply.github.com>
2022-06-16 03:17:58 +0530

    Update ping.py

--- handlers/ping.py
+++ handlers/ping.py
@@ -60,10 +60,10 @@ async def help(client: Client, message: Message):
             [
                 [
                     InlineKeyboardButton(
-                        "💖 sᴜᴘᴘᴏʀᴛ 💖", url=f"https://t.me/{SUPPORT_GROUP}"
+                        "💖 sᴜᴘᴘᴏʀᴛ 💖", url=f"https://t.me/+TFNht-Xwon1lM2Y1"
                     ),
                     InlineKeyboardButton(
-                        "🙄 sᴏᴜʀᴄᴇ 🙄", url="https://github.com/AnonymousR1025/FallenMusic"
+                        "🙄 ᴅᴇᴠᴇʟᴏᴘᴇʀ 🙄", url="https://t.me/zara_THE_addiction"
                     )
                 ]
             ]
